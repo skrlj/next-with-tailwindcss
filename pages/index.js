143475535b8ac22a1f161a2330df44f27f844ec2
James Skrlj <jskrlj1@nait.ca>
2022-11-01 14:53:19 -0600

    updated index.js

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,22 @@
+import Head from 'next/head'
+
 import { PageHeading } from "../components/page-heading"
 
 export default function Home() {
   return (
+    <>
+        <Head>
+          <meta charset="UTF-8"/>
+          <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+          <meta name="description" content="Tutorial on how to configure NEXT.js with tailwindcss."/>
+          <title>Learning React Vercel Deployment</title>
+        </Head>
+        
+        <PageHeading title="Deploying To Vercel"
+          tagline="setting up a continuous integration continuous deployment workflow."/>
+    </>
 
-    <PageHeading title="Deploying To Vercel"
-    tagline="setting up a continuous integration continuous deployment workflow."/>
      
   )
 }
